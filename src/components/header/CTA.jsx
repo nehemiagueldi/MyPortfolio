50ca6c7f72c12554e0bddd0589dda9069015fc2f
nehemiagueldi <nehemia.gueldi@student.umn.ac.id>
2022-11-07 13:22:54 +0700

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import CV from "../../assets/Nehemia-Gueldi-CV.pdf";
+// {CV}
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download="Nehemia Gueldi CV.pdf" className="btn">
+      <a href="https://drive.google.com/file/d/16kUKNyipX0wyogzt1r2lvXVYfIejHKR1/view?usp=sharing" target="_blank" className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
