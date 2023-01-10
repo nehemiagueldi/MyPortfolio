c36719bc434568f4638965d1ea31e163ff1e553c
nehemiagueldi <nehemia.gueldi@student.umn.ac.id>
2023-01-10 20:25:18 +0700

    Update Photo About Me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./about.css";
-import ME from "../../assets/PasFoto.jpeg";
+import ME from "../../assets/Photo2.png";
 import { FaAward } from "react-icons/fa";
 import { FiUsers } from "react-icons/fi";
 import { VscFolderLibrary } from "react-icons/vsc";
